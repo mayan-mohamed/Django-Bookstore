4b16c607a2a9cf044a647e1b18ab507c79428da3
mayan-mohamed <mayanmohamed506@gmail.com>
2025-01-28 00:39:01 +0200

    Removed unnecessary tracked files and directories

--- app/static/app/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('.book-cover');
-
-    images.forEach(img => {
-        img.addEventListener('error', function() {
-            this.src = this.dataset.placeholder; // to handle image errors
-        });
-    });
-});
